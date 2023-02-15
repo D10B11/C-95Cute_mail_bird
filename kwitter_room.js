2764a61376c6bb9d8cfa4026b32d6c3b70800dc8
D10B11 <115979875+D10B11@users.noreply.github.com>
2023-02-15 17:39:04 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAX039JipOFQ4tnC6YKWq28CxwgNTbmc6Y",
+    authDomain: "cute-mail-bird.firebaseapp.com",
+    projectId: "cute-mail-bird",
+    storageBucket: "cute-mail-bird.appspot.com",
+    messagingSenderId: "1048864969645",
+    appId: "1:1048864969645:web:eebe9429bf5ba6cf710724"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
